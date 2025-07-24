22002dcbee85a60cff82cdda95e3be495c6b6ece
Juan Narese <juanjo.narese@gmail.com>
2025-07-24 18:15:26 -0300

    envio de mail para recuperar contrasenia

--- helpers/mensajes.nodemailer.helper.js
+++ helpers/mensajes.nodemailer.helper.js
@@ -3,7 +3,7 @@ const { transporter } = require("../middlewares/nodemailer.middleware");
 const registroExitoso= async (emailUsuario, nombreUsuario)=> {
 try {
     
-    const info = await transporter.sendMail({
+    await transporter.sendMail({
     from: `"RollingVet" <${process.env.GMAIL_APP_USER}>`,
     to: `${emailUsuario}`,
     subject: "Registro exitoso ✔",
@@ -15,11 +15,61 @@ try {
     `, // HTML body
   });
 
+  
+  return{
+    msg: "ok",
+    statusCode: 200,
+  }  
+  
+  }
+
+ catch {
+   return{
+    error,
+    statusCode: 500,
+   
+   }
+
+  }
+}
+
+
+const recuperarContrasenia= async (token, emailUsuario)=> {
+try {
+    
+    const info = await transporter.sendMail({
+    from: `"RollingVet" <${process.env.GMAIL_APP_USER}>`,
+    to: `${emailUsuario}`,
+    subject: "Recuperacion contraseña ****",
+   
+    html: `
+    <b>Para ecuperar tu contraseña hace click en el link a continuacion:</b>
+    <a href="/recuperarConstrasenia"${token}>Ir a la página </a>
+    
+
+    
+    `, // HTML body
+  });
   console.log(info);
+  return{
+    msg: "ok",
+    statusCode: 200,
+  }
+    
+  
 }
 
  catch (error) {
-    console.log({error})
+  console.log({error})
+   return{
+    error,
+    statusCode: 500,
+   
+   }
+
 }
 }
-module.exports = {registroExitoso}
+
+
+
+module.exports = {registroExitoso, recuperarContrasenia}
