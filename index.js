0ccfce5afc9452cfc983d07e60ccf35913489955
Cuellar Facundo <f.cuellar2004@gmail.com>
2025-08-06 16:03:51 -0300

    turnero funcional testeado en postman

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ const cors = require("cors");
 const morgan = require("morgan");
 const path = require("path");
 
-app.use(express.json());
+app.use(express.json())
 app.use(cors());
 app.use(morgan("dev"));
 
-app.use("/turnero", require("./routes/turnero.routes"));
-app.use("/usuarios", require("./routes/usuarios.routes"));
+// app.use("/productos", require("./routes/productos.routes"))
+app.use("/usuarios", require("./routes/usuarios.routes"))
 // app.use("/carritos", require("./routes/carrito.routes"))
+app.use("/turnero", require("./routes/turnero.routes"))
 
-app.listen(8080, () => {
-  console.log("servidor funcionando puerto", 8080);
-});
+
+app.listen(8080, ()=>{
+    console.log("servidor funcionando puerto",8080)
+})
--- models/turnero.models.js
+++ models/turnero.models.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 
 const turnoSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
+  nombre: { type: String, trim: true, required: true},
   especie: {
     type: String,
+    trim: true,
     required: true,
     enum: ["perro", "gato", "ave", "otros"],
   },
-  raza: { type: String },
+  raza: { type: String,
+    trim: true
+   },
   detalleCita: { type: String },
   fecha: { type: Date },
   hora: { type: String },
