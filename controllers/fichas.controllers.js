e2f231546637dea72ec6d67dc167acf52467beb9
Cuellar Facundo <f.cuellar2004@gmail.com>
2025-08-07 16:58:04 -0300

    agregados errores

--- controllers/fichas.controllers.js
+++ controllers/fichas.controllers.js
@@ -7,28 +7,53 @@ const {
 } = require("../services/fichas.services.js")
 
 const obtenerFichas = async (req, res) => {
-    const {fichas, statusCode} = await obtenerFichasService()
-    res.status(statusCode).json({fichas})
+    try{
+        const {fichas, statusCode} = await obtenerFichasService()
+        res.status(statusCode).json({fichas})
+    }catch (error){
+        console.error('Error al obtener las fichas', error);
+        res.status(500).json({ msg: 'Error interno del servidor' });
+    }
 }
 
 const obtenerFichaPorID = async (req, res) => {
-    const {ficha, statusCode} = await obtenerFichaPorIDService(req.params.id)
-    res.status(statusCode).json({ficha})
+    try{
+        const {ficha, statusCode} = await obtenerFichaPorIDService(req.params.id)
+        res.status(statusCode).json({ficha})
+    }catch (error){
+        console.error('Error al obtener la ficha', error);
+        res.status(500).json({ msg: 'Error interno del servidor' });
+    }
 }
 
 const crearFicha = async (req, res) => {
-    const {msg, idFicha, statusCode} = await crearFichaService(req.body)
-    res.status(statusCode).json({msg, idFicha})
+    try{
+        const {msg, idFicha, statusCode} = await crearFichaService(req.body)
+        res.status(statusCode).json({msg, idFicha})
+    }catch (error){
+        console.error('Error al crear la ficha', error);
+        res.status(500).json({ msg: 'Error interno del servidor' });
+    }
 }
 
 const actualizarFicha = async (req, res) => {
-    const {msg, statusCode} = await actualizarFichaService(req.params.id, req.body)
-    res.status(statusCode).json({msg})
+    try{
+        const {msg, statusCode} = await actualizarFichaService(req.params.id, req.body)
+        res.status(statusCode).json({msg})
+    }catch (error){
+        console.error('Error al actualizar la fichas', error);
+        res.status(500).json({ msg: 'Error interno del servidor' });
+    }
 }
 
 const eliminarFicha = async (req, res) => {
-    const {msg, statusCode} = await eliminarFichaService(req.params.id)
-    res.status(statusCode).json({msg})
+    try{
+        const {msg, statusCode} = await eliminarFichaService(req.params.id)
+        res.status(statusCode).json({msg})
+    }catch (error){
+        console.error('Error al eliminar la fichas', error);
+        res.status(500).json({ msg: 'Error interno del servidor' });
+    }
 }
 
 module.exports = {
