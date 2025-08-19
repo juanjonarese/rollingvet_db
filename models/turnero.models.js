3610c8ccb6d341133932e5e776c5da727d67cb61
leonel salvatierra <leonelsalvatierra034@gmail.com>
2025-08-19 18:22:57 -0300

    correcion models

--- models/turnero.models.js
+++ models/turnero.models.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
 const turnoSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  especie: {
+  nombreMascota: { type: String, required: true },
+  /*   especie: {
     type: String,
     required: true,
     enum: ["perro", "gato", "ave", "otros"],
   },
-  raza: { type: String },
+  raza: { type: String }, */
 
   detalleCita: { type: String, required: true },
-  fecha: { type: Date },
-  hora: { type: String },
+  fecha: { type: Date, required: true },
+  hora: { type: String, required: true },
 });
 
 module.exports = mongoose.model("TurnoModel", turnoSchema);
