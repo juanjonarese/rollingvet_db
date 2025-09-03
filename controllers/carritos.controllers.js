46095ce2a69b7f407c4d37dbddb05066c38633f0
Juan <juanjo.narese@gmail.com>
2025-09-03 15:10:33 -0300

    carga de productos en carrito

--- controllers/carritos.controllers.js
+++ controllers/carritos.controllers.js
@@ -1,11 +1,55 @@
-const pagarCarritoMp = async (req, res) => {
- const {statusCode, msg, error} = await mercadoPagoServices();
-    try {
-        res.status(statusCode).json({ msg });
-    } catch {
-        res.status(statusCode).json({ error });
-    }
-}
-module.exports = {
-  pagarCarritoMp,
+const {
+  agregarProductosCarritoServices,
+  eliminarProductoCarritoIdServices,
+  obtenerTodosLosProductosDelCarritoServices,
+  mercadoPagoServices,
+} = require("../services/carritos.services");
+
+const obtenerTodosLosProductosDelCarrito = async (req, res) => {
+  const { msg, productos, error, statusCode } =
+    await obtenerTodosLosProductosDelCarritoServices(req.idCarrito);
+  try {
+    res.status(statusCode).json({ msg, productos });
+  } catch {
+    res.status(statusCode).json({ error });
   }
+};
+
+const agregarProductosCarrito = async (req, res) => {
+  const { msg, statusCode, error } = await agregarProductosCarritoServices(
+    req.idCarrito,
+    req.params.idProducto
+  );
+  try {
+    res.status(statusCode).json({ msg });
+  } catch {
+    res.status(statuCode).json({ error });
+  }
+};
+
+const eliminarProductoCarritioId = async (req, res) => {
+  const { msg, statusCode, error } = await eliminarProductoCarritoIdServices(
+    req.idCarrito,
+    req.params.idProducto
+  );
+  try {
+    res.status(statusCode).json({ msg });
+  } catch {
+    res.status(statusCode).json({ error });
+  }
+};
+
+const pagarCarritoMp = async (req, res) => {
+  const { statusCode, msg, error } = await mercadoPagoServices();
+  try {
+    res.status(statusCode).json({ msg });
+  } catch {
+    res.status(statusCode).json({ error });
+  }
+};
+module.exports = {
+  obtenerTodosLosProductosDelCarrito,
+  agregarProductosCarrito,
+  eliminarProductoCarritioId,
+  pagarCarritoMp,
+};
--- services/carritos.services.js
+++ services/carritos.services.js
@@ -1,6 +1,97 @@
-const mercadoPagoServices = async () => {
+const CarritosModel = require("../models/carrito.model");
+const ProductosModel = require("../models/productos.model");
+
+const { MercadoPagoConfig, Preference } = require("mercadopago");
+
+const obtenerTodosLosProductosDelCarritoServices = async (idCarrito) => {
   try {
-    const client = new MercadoPagoConfig({ accessToken: `${process.env.ACCESS_TOKEN_MP}` });
+    const carrito = await CarritosModel.findOne({ _id: idCarrito });
+    console.log(carrito.productos);
+    return {
+      productos: carrito.productos,
+      statusCode: 200,
+    };
+  } catch (error) {
+    return {
+      error,
+      statusCode: 500,
+    };
+  }
+};
+
+const agregarProductosCarritoServices = async (idCarrito, idProducto) => {
+  try {
+    const carrito = await CarritosModel.findOne({ _id: idCarrito });
+    const producto = await ProductosModel.findOne({ _id: idProducto });
+
+    const productoExiste = carrito.productos.find(
+      (prod) => prod._id.toString() === producto._id.toString()
+    );
+
+    if (productoExiste) {
+      return {
+        msg: "EL producto ya esta en el carrito",
+        statusCode: 422,
+      };
+    }
+
+    carrito.productos.push(producto);
+
+    await carrito.save();
+
+    return {
+      msg: "Producto cargado al carrito",
+      statusCode: 200,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      error,
+      statusCode: 500,
+    };
+  }
+};
+
+const eliminarProductoCarritoIdServices = async (idCarrito, idProducto) => {
+  try {
+    const carrito = await CarritosModel.findOne({ _id: idCarrito });
+
+    const productoExiste = carrito.productos.find(
+      (prod) => prod._id.toString() === idProducto
+    );
+
+    if (!productoExiste) {
+      return {
+        msg: "ERROR. EL producto que intentas borrar no existe",
+        statusCode: 404,
+      };
+    }
+
+    const productoIndex = carrito.productos.findIndex(
+      (prod) => prod._id.toString() === idProducto
+    );
+
+    carrito.productos.splice(productoIndex, 1);
+
+    await carrito.save();
+
+    return {
+      msg: "Producto eliminado",
+      statusCode: 200,
+    };
+  } catch (error) {
+    return {
+      error,
+      statusCode: 500,
+    };
+  }
+};
+
+const mercadoPagoServices = async (carrito) => {
+  try {
+    const client = new MercadoPagoConfig({
+      accessToken: `${process.env.ACCESS_TOKEN_MP}`,
+    });
 
     const preference = new Preference(client);
 
@@ -8,25 +99,35 @@ const mercadoPagoServices = async () => {
       body: {
         items: [
           {
-            title: 'Mi producto',
+            title: "Celular",
             quantity: 1,
             unit_price: 2000,
-            currency_id: 'ARS',
-          }
+            currency_id: "ARS",
+          },
+          {
+            title: "Smartv",
+            quantity: 1,
+            unit_price: 2000,
+            currency_id: "ARS",
+          },
         ],
         back_urls: {
-          success: 'Aqui la url del sitio web con el pago exitoso',
-          failure: 'Aqui la url del sitio web con el pago fallido',
-          pending: 'Aqui la url del sitio web con el pago pendiente'
-          // las backs urls deben estar deployadas desde el frontend
+          success: "https://localhost:5173/user/cart?success",
+          failure: "https://localhost:5173/user/cart?failure",
+          pending: "https://localhost:5173/user/cart?pending",
         },
-      }
+      },
     });
+
     console.log(res);
-    return{
-        msg:"Activado",
-        statusCode: 200,
-    }
+    /*  return {
+      msg: res.init_point,
+      statusCode: 200,
+    }; */
+    return {
+      msg: res.id,
+      statusCode: 200,
+    };
   } catch (error) {
     console.log(error);
     return {
@@ -37,5 +138,8 @@ const mercadoPagoServices = async () => {
 };
 
 module.exports = {
+  obtenerTodosLosProductosDelCarritoServices,
+  agregarProductosCarritoServices,
+  eliminarProductoCarritoIdServices, // ← corregir aquí también
   mercadoPagoServices,
 };
